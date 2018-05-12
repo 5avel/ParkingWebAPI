43e9bcd11ac2b16dc6185b5aa78bfc6dc38225b3
Bolubash Pavel <5avel@hotmail.com>
2018-05-12 13:48:42 +0300

    Transferring the code to Parking Class Library

---
+++ Parking/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Parking
+{
+    public class Car : ICloneable
+    {
+        private object syncRoot = new object();
+        public string LicensePlate { get; private set; }
+        private decimal balance;
+        public decimal Balance
+        {
+            get
+            {
+                lock (syncRoot)
+                {
+                    return balance;
+                }
+            }
+            set
+            {
+                lock (syncRoot)
+                {
+                    balance = value;
+                }
+            }
+        }
+        public CarType CarType { get; private set; }
+
+        public Car(string licensePlate, CarType carType, decimal balance = 0)
+        {
+            if (String.IsNullOrWhiteSpace(licensePlate)) throw new ArgumentException("");
+            this.LicensePlate = licensePlate;
+            this.CarType = carType;
+            this.Balance = balance;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ Parking/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Parking
+{
+    public class Transaction : ICloneable
+    {
+        public DateTime DateTime { get; private set; }
+        public string CarLicensePlate { get; private set; }
+        public decimal Debited { get; private set; }
+
+        public Transaction(string carLicensePlate, decimal debited)
+        {
+            this.DateTime = DateTime.Now;
+            this.CarLicensePlate = carLicensePlate;
+            this.Debited = debited;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
